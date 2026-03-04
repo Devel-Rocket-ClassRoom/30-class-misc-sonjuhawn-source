4ca52b1b06227543d4d42ad0f60926697196a0f1
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-04 22:31:39 +0900

    과제완료

--- QueryBuilder/Program.cs
+++ QueryBuilder/Program.cs
@@ -1,12 +1,23 @@
 using System;
+using System.Net.Http;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
 Console.WriteLine("=== 기본 쿼리 ===");
+QueryBuilder query1 = new QueryBuilder()
+    .Select("*")
+    .From("Users");
+Console.WriteLine(query1.Build());
+Console.WriteLine("=== 조건 쿼리 ===");
+QueryBuilder query2 = new QueryBuilder()
+    .Select("Name, Age")
+    .From("Users")
+    .Where("Age > 18");
+Console.WriteLine(query2.Build());
+Console.WriteLine("=== 정렬 쿼리 ===");
+QueryBuilder query3 = new QueryBuilder()
+    .Select("*")
+    .From("Products")
+    .Where("Price > 1000")
+    .OrderBy("Price");
+Console.WriteLine(query3.Build());  
 
-class QueryBuilder
-{
-    Select(string columns)
-    {
-
-    }
-}
---
+++ QueryBuilder/QueryBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class QueryBuilder
+{
+    private string query;
+    public QueryBuilder Select(string columns)
+    {
+        query += $"SELECT {columns}";
+        return this;
+    }
+    public QueryBuilder From(string table)
+    {
+        query += $"FROM {table}";
+        return this;
+    }
+    public QueryBuilder Where(string condition)
+    {
+        query += $"WHERE {condition}";
+        return this;
+    }
+    public QueryBuilder OrderBy(string column)
+    {
+        query += $"ORDER BY {column}";
+        return this;
+    }
+    public string Build()
+    {
+        return query;
+    }
+}
